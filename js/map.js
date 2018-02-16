0c9b4b3353a9cb230c3b12543fdb5365df0dce2b
xanalas <gign_91@mail.ru>
2018-02-16 03:21:01 +0300

    Работа над ошибками

--- js/map.js
+++ js/map.js
@@ -2,38 +2,56 @@
 
 // Объявляем переменные
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var TYPE = ['flat', 'house', 'bungalo'];
+var TYPE = ['flat', 'bungalo', 'house'];
 var TIME = ['12:00', '13:00', '14:00'];
 var FEAT = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var USERS = [1, 2, 3, 4, 5, 6, 7, 8];
 
 
-// Функция генерации случайных данных
+// ФУНЦИИ
+  // Функция генерации случайных данных
 var getRandomValue = function (arr) {
   var rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
 };
 
-
-// Возвращает случайное целое число между min (включительно) и max (не включая max)
+  // Функция возвращает случайное целое число между min (включительно) и max (не включая max)
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-
-// функция генерации случайной длины массива
+  // функция генерации случайной длины массива
 var getRandomLength = function (array) {
   var randomLenght = getRandomInt(1, array.length);
   return array.slice(0, randomLenght);
 };
 
+  // Частная функция которая присвоет случайному значению массива TYPE определение
+var getKey = function (key) {
+  if (key === 'flat') {
+    key = 'Квартира';
+  }
+  if (key === 'bungalo') {
+    key = 'Бунгало';
+  }
+  if (key === 'house') {
+    key = 'Дом';
+  }
+  return key;
+};
 
+
+// ОБЪЯВЛЯЕМ МАССИВ
 var markers = [];
-// Массив из объектов
+
+  // С помощью цикла сгенерировать в массив 8 объектов
 for (var i = 0; i <= 7; i++) {
+
+  // Получить случайные значения для Х и Y
   var x = getRandomInt(300, 901);
   var y = getRandomInt(150, 501);
 
+  // Добавить в массив объекты
   markers.push(
       {
         author: {
@@ -59,33 +77,35 @@ for (var i = 0; i <= 7; i++) {
           y: y
         }
       });
-
 }
 
 
-// У блока .map убираем класс .map-faded
+// РАБОТА С DOM-ДЕРЕВОМ
+// ГЕНЕРАЦИЯ МЕТОК
+  // У блока .map убираем класс .map-faded
 var userDialog = document.querySelector('.map');
 userDialog.classList.remove('map--faded');
 
-
-// Находим нужные элементы через querySelect
+  // Находим нужные элементы через querySelector
 var pins = userDialog.querySelector('.map__pins');
 var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 var mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
 
+  // Находим постоянные для метки
+var pinElem = pinTemplate.querySelector('img');
+var MAP_MARKER_WIDTH = pinElem.getAttribute('width') / 2;  //Ищем по атрибуту
+var MAP_MARKER_HEIGHT = parseFloat(pinElem.getAttribute('height'));
 
-// Функи=ция создания DOM-элементов
+  // Шаблон функцию для заполнения блока DOM-элементами на основе массива JS-объектов
 var createMapMarker = function (marker) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style.left = marker.location.x + 'px';
-  pinElement.style.top = marker.location.y + 'px';
+  pinElement.style.left = marker.location.x - MAP_MARKER_HEIGHT + 'px';
+  pinElement.style.top = marker.location.y - MAP_MARKER_WIDTH + 'px';
   pinElement.querySelector('img').src = marker.author.avatar;
-
   return pinElement;
 };
 
-
-// Функция генерации DOM-элементов
+  // функцию создания DOM-элемента на основе JS-объекта(с помощью ObjectFragment)
 var generateMapMarkers = function () {
   var fragment = document.createDocumentFragment();
 
@@ -95,12 +115,12 @@ var generateMapMarkers = function () {
   return pins.appendChild(fragment);
 };
 
+  // Вызываем функцию отрицовки
+generateMapMarkers(markers);
 
-// Вызываем функцию отрицовки
-generateMapMarkers(markers, pins);
 
-
-// Create Map Card
+// БЛАНК ОБЪЯВЛЕНИЯ
+  // Шаблон функцию для заполнения блока DOM-элементами на основе массива JS-объектов
 var createMapCard = function (post) {
   var mapCardElement = mapCardTemplate.cloneNode(true);
 
@@ -110,13 +130,13 @@ var createMapCard = function (post) {
 
   mapCardElement.querySelector('h3').textContent = post.offer.title;
   mapCardElement.querySelector('h3 + p > small').textContent = post.offer.address;
-  mapCardElement.querySelector('.popup__price').textContent = post.offer.price + '&#x20bd;/ночь';
-  mapCardElement.querySelector('h4').textContent = post.offer.type;
+  mapCardElement.querySelector('.popup__price').textContent = post.offer.price + '₽/ночь';
+  mapCardElement.querySelector('h4').textContent = getKey(post.offer.type);
   mapCardElement.querySelector('h4 + p').textContent = post.offer.rooms + ' для ' + post.offer.guests + ' гостей';
   mapCardElement.querySelector('h4 + p + p').textContent = 'Заезд после ' + post.offer.checkin + ' , выезд до ' + post.offer.checkout;
 
   for (var j = 0; j < post.offer.features.length; j++) {
-    featuresContainer.innerHTML += '<li class="feature feature--' + post.offer.features[i] + '"></li>';
+    featuresContainer.innerHTML += '<li class="feature feature--' + post.offer.features[j] + '"></li>';
   }
   mapCardElement.querySelector('.popup__features + p').textContent = post.offer.description;
   mapCardElement.querySelector('.popup__avatar').src = post.author.avatar;
@@ -124,18 +144,18 @@ var createMapCard = function (post) {
   return mapCardElement;
 };
 
-// Output Map Card
+  // функцию создания DOM-элемента на основе JS-объекта(с помощью ObjectFragment)
 var outputMapCard = function () {
   var mapCardFragment = document.createDocumentFragment();
 
-  mapCardFragment.appendChild(createMapCard(markers[0]));
+  mapCardFragment.appendChild(createMapCard(markers[0])); // Используем только первый объект массива
 
   var statement = document.querySelector('.map');
 
   return statement.insertBefore(mapCardFragment, statement.querySelector('.map__filters-container'));
 };
 
+ // Вызываем функцию отрицовки бланка
 outputMapCard(markers);
 
-/* pinElement.style.left = marker.location.x - MAP_MARKER_HEIGHT + 'px';
-pinElement.style.top = marker.location.y - MAP_MARKER_WIDTH / 2 + 'px';*/
+
